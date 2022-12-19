824b2903c47db2ad75131a146560623b1611c7b6
aslam48 <youngfizzy001@gmail.com>
2022-12-19 14:54:19 +0100

    working

--- src/Component/skills/Skills.jsx
+++ src/Component/skills/Skills.jsx
@@ -8,8 +8,9 @@ const Skills = () => {
             <h2 className='section__title'>Skills</h2>
             <span className='section__subtitle'>My Technical Level </span>
 
-            <div className="skills__container container grid"></div>
+            <div className="skills__container container grid">
             <FrontEnd/>
+            </div>
     </section>
   )
 }
